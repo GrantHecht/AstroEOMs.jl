54ba7a5212c79d3a36287098e5349b389e1a60ea
Documenter.jl <documenter@juliadocs.github.io>
2022-10-05 13:24:17 +0000

    build based on 5278ad9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AstroEOMs","category":"page"},{"location":"#AstroEOMs","page":"Home","title":"AstroEOMs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AstroEOMs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AstroEOMs]","category":"page"}]
+}
